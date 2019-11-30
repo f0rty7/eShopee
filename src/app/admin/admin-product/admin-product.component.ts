2ea3ec5b2b96f8c39734b83f61b80923b226685e
Hem Sunder <hemsunder01@gmail.com>
2019-11-30 10:30:52 +0530

    search bar filtering done

--- src/app/admin/admin-product/admin-product.component.ts
+++ src/app/admin/admin-product/admin-product.component.ts
@@ -1,22 +1,34 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-product',
   templateUrl: './admin-product.component.html',
   styleUrls: ['./admin-product.component.css']
 })
-export class AdminProductComponent implements OnInit {
+export class AdminProductComponent implements OnInit, OnDestroy {
 
-  product;
-  constructor(private productService: ProductService) {
+  product: { title: string }[];
+  subscription: Subscription;
+  filteredProducts: any[];
+  constructor(private productService: ProductService) { }
+
+  filter(query: string){
+    // console.log(query);
+    this.filteredProducts = (query) ?
+    this.product.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.product
   }
 
   ngOnInit() {
-    this.productService.product$
+    this.subscription =this.productService.product$
     .subscribe( x => {
-      this.product = x;
+      this.filteredProducts = this.product = x;
     })
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
