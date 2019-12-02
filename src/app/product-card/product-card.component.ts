beefb3ffeb6e5042f9c51f4c75943fdf2309074a
Hem Sunder <hemsunder01@gmail.com>
2019-12-02 09:25:30 +0530

    shopping cart stable adding diff products done

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -15,16 +15,7 @@ export class ProductCardComponent{
   constructor(private cartService: ShoppingCartService) { }
 
   addToCart(product: Product){
-    let cartId = localStorage.getItem('cartId');
-    if( !cartId ){
-      this.cartService.create().then(result => {
-        localStorage.setItem('cartId', result.key)
-
-        //Add product to cart
-      });
-    } else {
-      //add peoduct to cart
-    }
+    this.cartService.addToCart(product);
   }
 
 }
--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,5 +1,7 @@
+import { Product } from './models/product';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,34 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  create(){
+  private create(){
     return this.db.list('/shopping-carts').push({
       dateCreated: new Date().getTime()
     });
+  }
+
+  private getCart(cartId: string){
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCartId(){
+    let cartId = localStorage.getItem('cartId');
+    if(cartId) return cartId;
+    
+    let result = await this.create();
+    localStorage.setItem('cartId', result.key)
+    return result.key;
+  }
+
+  async addToCart(product: Product){
+    let cartId = await this.getOrCreateCartId();
+    let item$ = this.db.object('/shopping-carts/' + cartId + '/items/' + product.key);
+    item$.snapshotChanges().pipe(first()).subscribe(item => {
+      if (item.payload.exists()) {
+      item$.update({
+      quantity: item.payload.exportVal().quantity + 1
+    });
+    } else item$.set({product: product, quantity: 1});
+  });
   }
 }
