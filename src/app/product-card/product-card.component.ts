368d201f49d7ac9cb99fbbc6609eb23b670456fb
Hem Sunder <hemsunder01@gmail.com>
2019-12-04 03:48:40 +0530

    getquantity() fix required

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,3 +1,4 @@
+import { AngularFireDatabase } from '@angular/fire/database';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { Product } from './../models/product';
 import { Component, Input } from '@angular/core';
@@ -11,11 +12,21 @@ export class ProductCardComponent{
 
   @Input('product') product: Product;
   @Input('show-actions') showActions = true;
+  @Input('shopping-cart') shoppingCart;
 
-  constructor(private cartService: ShoppingCartService) { }
+  constructor(private cartService: ShoppingCartService,db: AngularFireDatabase) { }
 
   addToCart(product: Product){
     this.cartService.addToCart(product);
+  }
+
+  getQuantity(){
+    if(!this.shoppingCart) return 0;
+    // console.log("heyllo",this.shoppingCart)
+    // let item = this.shoppingCart[this.product.key];
+    let item = this.shoppingCart[this.product.key]
+    // console.log("product is", this.product);
+    return item ? item.quantity : 0;
   }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,26 +1,31 @@
+import { ShoppingCartService } from './../shopping-cart.service';
 import { switchMap } from 'rxjs/operators';
 import { Product } from './../models/product';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from './../product.service';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
   //products: any[] = [];
   filteredProducts: Product[] = [];
   category$;
   category: string;
-  //cat: string;
+  cart: any;
+  subscription: Subscription;
   constructor(
     route: ActivatedRoute,
-    productService: ProductService) {
-    productService.product$.pipe(switchMap( products => { 
+    productService: ProductService,
+    private shoppingCartSerive: ShoppingCartService,) {
+          
+      productService.product$.pipe(switchMap( products => { 
       this.products = products;
       return route.queryParamMap
     }))
@@ -30,6 +35,15 @@ export class ProductsComponent {
         this.filteredProducts = (this.category) ?
           this.products.filter( p => p.category === this.category ) : this.products
       });
+  }
+
+  async ngOnInit(){
+    this.subscription = (await this.shoppingCartSerive.getCart()).valueChanges().subscribe(cart => {this.cart = cart;
+    console.log("cart value is ",cart)});
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
