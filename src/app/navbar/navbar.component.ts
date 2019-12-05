92eb6b5804481dda5223f5304479d9ff7fa706ed
Hem Sunder <hemsunder01@gmail.com>
2019-12-06 02:09:29 +0530

    shopping cart product display done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
+import { ShoppingCartItem } from './../models/shopping-cart-item';
 import { ShoppingCartService } from './../shopping-cart.service';
 import { AppUser } from './../models/app-user';
 import { AuthService } from './../auth.service';
 import { Component,OnInit } from '@angular/core';
 import { count } from 'rxjs/operators';
+import { ShoppingCart } from '../models/shopping-cart';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -12,6 +14,7 @@ export class NavbarComponent {
 
   appUser: AppUser;
   shoppingCartItemCount: number;
+  // items: ShoppingCart<ShoppingCartItem[]>;
 
   constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { 
 
@@ -23,7 +26,7 @@ export class NavbarComponent {
     cart$.valueChanges().subscribe( cart => {
       console.log(cart)
       this.shoppingCartItemCount = 0;
-      const items = cart.items;
+      const items = cart['items'];
       for ( let productId in items)
         this.shoppingCartItemCount += items[productId].quantity;
     });
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { ShoppingCart } from "./../models/shopping-cart";
+import { ShoppingCartService } from "./../shopping-cart.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-shopping-cart',
-  templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.css']
+  selector: "shopping-cart",
+  templateUrl: "./shopping-cart.component.html",
+  styleUrls: ["./shopping-cart.component.css"]
 })
 export class ShoppingCartComponent implements OnInit {
+  cart$;
+  totalItemsCount: number = 0;
+  items1: ShoppingCart;
+  productDetails: any[] = [];
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.cart$ = await this.shoppingCartService.getCart();
+    this.cart$.valueChanges().subscribe(x => {
+      console.log({ x });
+      for (let productId in x["items"]) {
+        this.totalItemsCount += x["items"][productId].quantity;
+      }
+      this.getProductIds(x);
+    });
   }
 
+  getProductIds(items) {
+    console.log("ghggh", items);
+    // return Object.keys(this.items1);
+    for (let product in items["items"]) {
+      this.productDetails.push({
+        title: items["items"][product].product.title,
+        quantity: items["items"][product].quantity
+      });
+    }
+    console.log(this.productDetails);
+  }
 }
